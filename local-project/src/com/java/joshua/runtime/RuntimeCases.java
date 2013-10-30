dd5cfa465d234813ce033a6acb71c1010804f01d
Joshua Zhang <joshua.zhs@gmail.com>
2013-10-30 15:21:42 +0800

    first commit

---
+++ local-project/src/com/java/joshua/runtime/RuntimeCases.java
@@ -0,0 +1,11 @@
+package com.java.joshua.runtime;
+
+public class RuntimeCases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int processorCount = Runtime.getRuntime().availableProcessors();
+		System.out.println(processorCount);
+	}
+
+}
